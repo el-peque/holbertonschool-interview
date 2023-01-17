8ec013f0806befc918050bbd97bc29c35ed947cc
Peque <ezequielcabrera2601@gmail.com>
2023-01-17 18:01:02 -0300

    Add task 0

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: linked list head
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *aux, *dummy;
+	listint_t *one_step = *head, *two_step = *head;
+
+	if (!head || !one_step->next)
+		return (1);
+	aux = malloc(sizeof(listint_t **));
+	dummy = malloc(sizeof(listint_t **));
+	while (one_step)
+	{
+		if (two_step->next)
+		{
+			aux = malloc(sizeof(listint_t **));
+			aux->next = dummy;
+			aux->n = one_step->n;
+			dummy = aux;
+		}
+		if (!two_step->next || !two_step->next->next)
+		{
+			one_step = one_step->next;
+			while (dummy->next && one_step->next)
+			{
+				if (dummy->n != one_step->n)
+					return (0);
+				dummy = dummy->next;
+				one_step = one_step->next;
+			}
+			if (dummy->n != one_step->n)
+				return (0);
+			return (1);
+		}
+		one_step = one_step->next;
+		two_step = two_step->next->next;
+	}
+	return (0);
+}
