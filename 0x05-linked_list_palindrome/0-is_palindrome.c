01c93601cbe857f3a0325ed2e1734cda7932c880
Peque <ezequielcabrera2601@gmail.com>
2023-01-19 14:05:37 -0300

    Fixed memory leaks

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -9,38 +9,42 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *aux, *dummy;
+	listint_t *aux, *dummy, *dummy_head;
 	listint_t *one_step = *head, *two_step = *head;
 
 	if (!head || !one_step->next)
 		return (1);
-	aux = malloc(sizeof(listint_t **));
 	dummy = malloc(sizeof(listint_t **));
+	dummy_head = dummy;
 	while (one_step)
 	{
 		if (two_step->next)
 		{
 			aux = malloc(sizeof(listint_t **));
-			aux->next = dummy;
-			aux->n = one_step->n;
-			dummy = aux;
-		}
+			aux->next = dummy, aux->n = one_step->n;
+			dummy = aux; }
 		if (!two_step->next || !two_step->next->next)
 		{
+			aux = dummy;
 			one_step = one_step->next;
 			while (dummy->next && one_step->next)
 			{
 				if (dummy->n != one_step->n)
-					return (0);
+				{
+					free_listint(aux);
+					return (0); }
 				dummy = dummy->next;
 				one_step = one_step->next;
 			}
 			if (dummy->n != one_step->n)
-				return (0);
+			{
+				free_listint(aux);
+				return (0); }
+			free_listint(aux);
 			return (1);
 		}
-		one_step = one_step->next;
-		two_step = two_step->next->next;
+		one_step = one_step->next, two_step = two_step->next->next;
 	}
+	free_listint(dummy);
 	return (0);
 }
