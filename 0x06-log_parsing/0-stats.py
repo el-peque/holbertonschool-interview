07bbbbf83b860ea3e37079cae6c6a6a18ad48b6c
Peque <ezequielcabrera2601@gmail.com>
2023-01-25 12:20:05 -0300

    Fix task 0

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -5,21 +5,20 @@ import sys
 
 if __name__ == '__main__':
     i, j = 0, 0
+    stats = {'200': 0, '301': 0, '400': 0, '401': 0,
+             '403': 0, '404': 0, '405': 0, '500': 0}
     line_count = 0
     total_size = 0
     status_code = []
     for line in sys.stdin:
-        stats = line.split()
-        if stats[-2] and (stats[-2]).isdigit():
-            status_code.append(stats[-2])
-        file_size = int(stats[-1])
+        line_tok = line.split()
+        if line_tok[-2] in stats:
+            stats[line_tok[-2]] += 1
+        file_size = int(line_tok[-1])
         total_size += file_size
         if line_count % 10 == 0 and line_count > 0:
             print("File size: {}".format(total_size))
-            i = j
-            sorted_status = sorted(status_code[i:])
-            for i, status in enumerate(sorted_status):
-                if status not in sorted_status[i+1:]:
-                    print("{}: {}".format(status, status_code.count(status)))
-                j += 1
+            for code, count in stats.items():
+                if count > 0:
+                    print("{}: {}".format(code, count))
         line_count += 1
