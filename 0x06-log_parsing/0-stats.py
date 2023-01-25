7e7ab8eca60ae8cc5ed73c7400b13708bae1ba71
Peque <ezequielcabrera2601@gmail.com>
2023-01-25 14:29:34 -0300

    Fix task 0

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,8 +3,15 @@
 import sys
 
 
+def line_parser(stats):
+    if not line or len(stats) != 9 or stats[1] != '-':
+        return False, 0, 0
+    if not stats[-1].isdigit() or not stats[-2].isdigit():
+        return False, 0, 0
+    return True, stats[-2], int(stats[-1])
+
+
 if __name__ == '__main__':
-    i, j = 0, 0
     stats = {'200': 0, '301': 0, '400': 0, '401': 0,
              '403': 0, '404': 0, '405': 0, '500': 0}
     line_count = 0
@@ -12,9 +19,11 @@ if __name__ == '__main__':
     status_code = []
     for line in sys.stdin:
         line_tok = line.split()
-        if line_tok[-2] in stats:
-            stats[line_tok[-2]] += 1
-        file_size = int(line_tok[-1])
+        valid, status_code, file_size = line_parser(line_tok)
+        if not valid:
+            continue
+        if status_code in stats.keys():
+            stats[status_code] += 1
         total_size += file_size
         if line_count % 10 == 0 and line_count > 0:
             print("File size: {}".format(total_size))
