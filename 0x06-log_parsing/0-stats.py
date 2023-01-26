10c5b44cff1c578dc632dbe6c7430ac4a84426ff
Peque <ezequielcabrera2601@gmail.com>
2023-01-26 14:26:31 -0300

    Add task 0

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,36 +1,42 @@
 #!/usr/bin/python3
 """Log parsing"""
 import sys
+stats = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
 
 
-def print_parsed_stats(stats, total_size):
+def print_parsed_stats(total_size):
+    """Prints stast in '{status_code}: {number}' format"""
     print("File size: {}".format(total_size))
     for code in sorted(stats.keys()):
         if stats[code] > 0:
             print("{}: {}".format(code, stats[code]))
 
 
+def parse_line(line):
+    """Parses line and return file size"""
+    try:
+        line_tok = line.split()
+        status_code = line_tok[-2]
+        file_size = int(line_tok[-1])
+        if status_code in stats.keys():
+            stats[status_code] += 1
+        return file_size
+    except Exception:
+        return 0
+
+
 if __name__ == '__main__':
-    stats = {'200': 0, '301': 0, '400': 0, '401': 0,
-             '403': 0, '404': 0, '405': 0, '500': 0}
     line_count = 0
     total_size = 0
-    status_code = []
     try:
         for line in sys.stdin:
-            line_tok = line.split()
-            try:
-                status_code = line_tok[-2]
-                file_size = int(line_tok[-1])
-            except Exception:
-                continue
-            if status_code in stats.keys():
-                stats[status_code] += 1
+            file_size = parse_line(line)
             total_size += file_size
             if line_count % 10 == 0 and line_count > 0:
-                print_parsed_stats(stats, total_size)
+                print_parsed_stats(total_size)
             line_count += 1
     except KeyboardInterrupt:
-        print_parsed_stats(stats, total_size)
+        print_parsed_stats(total_size)
         raise
-    print_parsed_stats(stats, total_size)
+    print_parsed_stats(total_size)
