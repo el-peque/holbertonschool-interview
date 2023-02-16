f61e42b11bc50fd6c1e79af614c553b8d2a89dd8
Peque <ezequielcabrera2601@gmail.com>
2023-02-16 11:29:19 -0300

    Fix task 0

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -5,27 +5,23 @@
 def validUTF8(data):
     """Determines if a given data set represents a valid UTF-8 encoding"""
     binary = []
+    byte = 0
     for i, value in enumerate(data):
-        binary.append(to_binary(value))
-        if binary[0][:1] == '0' and binary[i][:1] != '0':
+        binary = to_binary(value)
+        if byte > 0 and binary[:2] != [1, 0]:
             return False
-        if binary[0][:3] == '110' and i != 0 and binary[i][:2] != '10':
-            return False
-        if binary[0][:4] == '1110' and i != 0 and binary[i][:2] != '10':
-            return False
-        if binary[0][:5] == '11110' and i != 0 and binary[i][:2] != '10':
-            return False
+        if byte == 0:
+            byte = binary.index(0)
+        byte -= 1
     return True
 
 
 def to_binary(n):
     """Converts n to its binary representation"""
     mult = [128, 64, 32, 16, 8, 4, 2, 1]
-    binary = ""
+    binary = [0] * 8
     for i, e in enumerate(mult):
         if n - e >= 0:
             n -= e
-            binary += '1'
-            continue
-        binary += '0'
+            binary[i] = 1
     return binary
