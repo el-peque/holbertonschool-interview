67ea84aa111eb4d74a2869a4624f61b3fca46ff0
Peque <ezequielcabrera2601@gmail.com>
2023-02-21 16:09:18 -0300

    Fix task 0

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -12,70 +12,19 @@
  */
 int slide_line(int *line, size_t size, int direction)
 {
-	size_t i = 0;
 	int change = 0;
 	int changes[size];
 
 	fill_zeroes(changes, size);
 	while (direction == SLIDE_RIGHT)
 	{
-		change = 0;
-		for (i = size - 1; i > 0; i--)
-		{
-			if (i > 0 && line[i - 1] != 0)
-			{
-				if (line[i] == 0)
-				{
-					line[i] += line[i - 1];
-					line[i - 1] = 0;
-					change++;
-					if (changes[i - 1] == 1)
-					{
-						changes[i] = 1;
-						changes[i - 1] = 0;
-					}
-				}
-				else if (changes[i - 1] != 1 && line[i - 1] == line[i])
-				{
-					changes[i] = 1;
-					changes[i - 1] = 0;
-					line[i] += line[i - 1];
-					line[i - 1] = 0;
-					change++;
-				}
-			}
-		}
+		change = slide_right(line, changes, size);
 		if (change == 0)
 			return (1);
 	}
 	while (direction == SLIDE_LEFT)
 	{
-		change = 0;
-		for (i = 0; i < size; i++)
-		{
-			if (i < size - 1 && line[i + 1] != 0)
-			{
-				if (line[i] == 0)
-				{
-					line[i] += line[i + 1];
-					line[i + 1] = 0;
-					change++;
-					if (changes[i] == 1)
-					{
-						changes[i] = 1;
-						changes[i + 1] = 0;
-					}
-				}
-				else if (changes[i] != 1 && line[i + 1] == line[i])
-				{
-					changes[i] = 1;
-					changes[i + 1] = 0;
-					line[i] += line[i];
-					line[i + 1] = 0;
-					change++;
-				}
-			}
-		}
+		change = slide_left(line, changes, size);
 		if (change == 0)
 			return (1);
 	}
@@ -83,11 +32,94 @@ int slide_line(int *line, size_t size, int direction)
 }
 
 /**
+ * slide_right - Reproduces the 2048 right move on a single horizontal line
+ *
+ * @line: Pointer to the line
+ * @changes: Pointer to changes
+ * @size: Number of elements in @line
+ *
+ * Return: number of changes to line
+ */
+int slide_right(int *line, int *changes, size_t size)
+{
+	int i = 0, change = 0;
+
+	for (i = size - 1; i > 0; i--)
+	{
+		if (i > 0 && line[i - 1] != 0)
+		{
+			if (line[i] == 0)
+			{
+				line[i] += line[i - 1];
+				line[i - 1] = 0;
+				change++;
+				if (changes[i - 1] == 1)
+				{
+					changes[i] = 1;
+					changes[i - 1] = 0;
+				}
+			}
+			else if (changes[i - 1] != 1 && line[i - 1] == line[i])
+			{
+				changes[i] = 1;
+				changes[i - 1] = 0;
+				line[i] += line[i - 1];
+				line[i - 1] = 0;
+				change++;
+			}
+		}
+	}
+	return (change);
+}
+
+/**
+ * slide_left - Reproduces the 2048 left move on a single horizontal line
+ *
+ * @line: Pointer to the line
+ * @changes: Pointer to changes
+ * @size: Number of elements in @line
+ *
+ * Return: number of changes to line
+ */
+int slide_left(int *line, int *changes, size_t size)
+{
+	int change = 0;
+	size_t i = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		if (i < size - 1 && line[i + 1] != 0)
+		{
+			if (line[i] == 0)
+			{
+				line[i] += line[i + 1];
+				line[i + 1] = 0;
+				change++;
+				if (changes[i] == 1)
+				{
+					changes[i] = 1;
+					changes[i + 1] = 0;
+				}
+			}
+			else if (changes[i] != 1 && line[i + 1] == line[i])
+			{
+				changes[i] = 1;
+				changes[i + 1] = 0;
+				line[i] += line[i];
+				line[i + 1] = 0;
+				change++;
+			}
+		}
+	}
+	return (change);
+}
+
+/**
  * fill_zeroes - Fills an array with 0
  *
  * @array: pointer to the array to fill
  * @size: size of the array
- */ 
+ */
 void fill_zeroes(int *array, size_t size)
 {
 	size_t i = 0;
--- 0x0A-slide_line/slide_line.h
+++ 0x0A-slide_line/slide_line.h
@@ -1,12 +1,14 @@
 #ifndef SLIDE_LINE_H
-#define SLINE_LINE_H
+#define SLIDE_LINE_H
 
+#include <stddef.h>
 #define SLIDE_LEFT 0
 #define SLIDE_RIGHT 1
 
-#include <stddef.h>
 
 int slide_line(int *line, size_t size, int direction);
+int slide_right(int *line, int *changes, size_t size);
+int slide_left(int *line, int *changes, size_t size);
 void fill_zeroes(int *array, size_t size);
 
 #endif /* SLIDE_LINE_H */
