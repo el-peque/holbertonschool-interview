51b4dd1938240d6e5952288ad6ea18b204fb9f05
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2023-02-22 10:02:20 -0300

    Update slide_line.h

--- 0x0A-slide_line/slide_line.h
+++ 0x0A-slide_line/slide_line.h
@@ -7,7 +7,6 @@
 #define SLIDE_LEFT 0
 #define SLIDE_RIGHT 1
 
-
 int slide_line(int *line, size_t size, int direction);
 int slide_right(int *line, int *changes, size_t size);
 int slide_left(int *line, int *changes, size_t size);
