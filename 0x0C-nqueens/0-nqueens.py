8936981f318d5550b1c2b046f8a0603159132c83
Peque <ezequielcabrera2601@gmail.com>
2023-03-23 22:16:49 -0300

    Initial commit

---
+++ 0x0C-nqueens/0-nqueens.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""N queens"""
+import sys
+
+
+if len(sys.argv) != 2:
+    print('Usage: nqueens N')
+    exit(1)
+
+if not sys.argv[1].isdigit():
+    print('N must be a number')
+    exit(1)
+N = int(sys.argv[1])
+
+if N < 4:
+    print('N must be at least 4')
+    exit(1)
