5166c567b685a0864102ff81f8e4f33acd8970b5
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2023-05-20 18:51:14 +0000

    Fix task 0

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -4,7 +4,7 @@ const request = require('request');
 const process = require('process');
 
 const filmNumber = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/' + filmNumber + '?format=json';
+const url = 'https://swapi-api.hbtn.io/api/films/' + filmNumber;
 
 request(url, function (error, response, body) {
   if (error || response.statusCode !== 200) return;
