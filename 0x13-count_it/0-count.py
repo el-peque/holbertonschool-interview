a9ab3c51818b85c37d8c45224e2d6509c9dce3b7
Peque <ezequielcabrera2601@gmail.com>
2023-05-20 17:52:03 -0300

    Fixed it

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -2,15 +2,16 @@
 """Count it!"""
 import requests
 import sys
-import json
 
 
 def count_words(subreddit, word_list):
     """Count words in subreddit"""
     r = requests.get('https://www.reddit.com/r/{}/hot.json'.format(subreddit))
-    data = r.json().get('data')
-    if not data:
+    print(r.status_code)
+    if r.status_code != '200':
         return
+
+    data = r.json().get('data')
     children = data.get('children')
     word_count = {k: v for k, v in zip(
         [word.lower() for word in word_list], [0] * len(word_list))}
@@ -18,9 +19,11 @@ def count_words(subreddit, word_list):
         title = child.get('data').get('title').lower()
         for word in word_count.keys():
             word_count[word] += title.count(word)
+
     if sum(word_count.values()) == 0:
         return
-    for k, v in word_count.items():
+    sorted_word_count = sorted(word_count.items(), key=lambda x: x[1])
+    for k, v in sorted_word_count.items():
         print('{}: {}'.format(k, v))
 
 
