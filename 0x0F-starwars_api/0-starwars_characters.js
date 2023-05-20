928806e27b9670fe74de7d9c815a3e04436e93af
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2023-05-20 18:58:11 +0000

    Fix task 0

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -3,17 +3,32 @@
 const request = require('request');
 const process = require('process');
 
-const filmNumber = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/' + filmNumber;
-
-request(url, function (error, response, body) {
-  if (error || response.statusCode !== 200) return;
-  const info = JSON.parse(body);
-  info.characters.forEach(character => {
-    request(character, (error, response, body) => {
-      if (error || response.statusCode !== 200) return;
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
+function getRequest(url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        reject(error || new Error(`Request failed with status code ${response.statusCode}`));
+      } else {
+        resolve(JSON.parse(body));
+      }
     });
   });
-});
+}
+
+async function getMovieCharacters(filmNumber) {
+  const url = 'https://swapi-api.hbtn.io/api/films/' + filmNumber;
+
+  try {
+    const movieData = await getRequest(url);
+    const characterPromises = movieData.characters.map(characterUrl => getRequest(characterUrl));
+    const characterDataList = await Promise.all(characterPromises);
+
+    characterDataList.forEach(characterData => {
+      console.log(characterData.name);
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+getMovieCharacters(process.argv[2]);
