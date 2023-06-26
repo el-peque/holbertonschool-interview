b47fae00865998c7cffe06ec7ab4005869cb9cfc
Peque <ezequielcabrera2601@gmail.com>
2023-06-26 17:21:14 -0300

    Fixed betty-style

--- sorted_array_to_avl/0-sorted_array_to_avl.c
+++ sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -1,6 +1,21 @@
 #include "binary_trees.h"
 
 /**
+ * sorted_array_to_avl - builds an AVL tree from an array
+ * @array: pointer to the first element of the array to be converted
+ * @size: number of element in the array
+ * Return: pointer to the root node of the created AVL tree, or NULL on failure
+ */
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+	if (!array)
+		return (NULL);
+
+	return (create_avl_tree(array, 0, size - 1, NULL));
+
+}
+
+/**
  * create_avl_tree - builds an AVL tree from an array
  * @array: pointer to the first element of the array to be converted
  * @start: start index of the subarray
@@ -17,7 +32,7 @@ avl_t *create_avl_tree(int *array, int start, int end, avl_t *parent)
 		return (NULL);
 
 	mid = (start + end) / 2;
-	
+
 	root = malloc(sizeof(avl_t));
 	if (!root)
 		return (NULL);
@@ -29,50 +44,3 @@ avl_t *create_avl_tree(int *array, int start, int end, avl_t *parent)
 
 	return (root);
 }
-
-/**
- * sorted_array_to_avl - builds an AVL tree from an array
- * @array: pointer to the first element of the array to be converted
- * @size: number of element in the array
- * Return: pointer to the root node of the created AVL tree, or NULL on failure
- */
-avl_t *sorted_array_to_avl(int *array, size_t size)
-{
-	if (!array)
-		return (NULL);
-
-	return (create_avl_tree(array, 0, size - 1, NULL));
-
-}
-
-
-
-/*
-	// size_t i = 0;
-	// avl_t *head = NULL;
-
-	// if (!array || size == 0)
-	// 	return (NULL);
-
-	// head = malloc(sizeof(avl_t));
-	// if (!head)
-	// 	return (NULL);
-
-	// for (i = 0; i < size; i++)
-	// {
-	// 	if (!head->left)
-	// 		head->n = array[i];
-	// 	else if (!head->right)
-	// 	{
-	// 		head->right = malloc(sizeof(avl_t));
-	// 		head->right->n = array[i];
-	// 	}
-	// 	else
-	// 	{
-	// 		head->parent->n = array[i];
-	// 		head = head->parent;
-	// 	}
-	// }
-
-	// return (head);
-	*/
