664aec48c803369057f86291fb6d20c13864c2ac
Peque <ezequielcabrera2601@gmail.com>
2023-08-21 17:19:34 -0300

    Fixed error

--- count_it/0-count.py
+++ count_it/0-count.py
@@ -12,9 +12,9 @@ def count_words(subreddit, word_list):
     def count_words_recursive(subreddit, word_list, after=None):
         """Count words recursively"""
         if after:
-            params = {"limit": 100, "after": after}
+            params = {"after": after}
         else:
-            params = {"limit": 100}
+            params = {}
 
         r = requests.get(url='https://www.reddit.com/r/{}/hot.json'.format(
             subreddit),
@@ -39,7 +39,7 @@ def count_words(subreddit, word_list):
     if sum(word_count.values()) == 0:
         return
     sorted_word_count = sorted(
-        word_count.items(), key=lambda x: x[1], reverse=True)
+        word_count.items(), key=lambda x: (-x[1], x[0].lower()))
     for item in sorted_word_count:
         if item[1] != 0:
             print('{}: {}'.format(item[0], item[1]))
