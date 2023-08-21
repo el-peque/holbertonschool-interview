93fe34cafd3a2e9be4f58a0842d1b2decf49828b
Peque <ezequielcabrera2601@gmail.com>
2023-08-21 17:00:31 -0300

    Fixed error

--- count_it/0-count.py
+++ count_it/0-count.py
@@ -1,50 +1,50 @@
 #!/usr/bin/python3
-"""0 - Count it! - 0"""
+"""Count it!"""
 import requests
+import sys
 
 
-def count_words(subreddit, word_list, after=None, counts=None):
+def count_words(subreddit, word_list):
     """Count words in subreddit"""
-    if counts is None:
-        counts = {word.lower(): 0 for word in word_list}
-    if after is None:
-        after = ''
+    word_count = {k: v for k, v in zip(
+        [word.lower() for word in word_list], [0] * len(word_list))}
 
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    def count_words_recursive(subreddit, word_list, after=None):
+        """Count words recursively"""
+        if after:
+            params = {"limit": 100, "after": after}
+        else:
+            params = {"limit": 100}
 
-    params = {"limit": 100, "after": after}
+        r = requests.get(url='https://www.reddit.com/r/{}/hot.json'.format(
+            subreddit),
+            params=params,
+            headers={'User-Agent': 'Mozilla/5.0'})
+        if r.status_code != 200:
+            return
 
-    r = requests.get(url, params=params, headers={'User-Agent': 'Mozilla/5.0'})
+        data = r.json().get('data')
+        children = data.get('children')
 
-    if r.status_code != 200:
-        print("No posts match or the subreddit is invalid.")
+        for child in children:
+            title = child.get('data').get('title').lower()
+            for word in word_count.keys():
+                word_count[word] += title.count(word)
+
+        after = data.get('after')
+        if after:
+            count_words_recursive(subreddit, word_list, after)
+
+    count_words_recursive(subreddit, word_list)
+
+    if sum(word_count.values()) == 0:
         return
-
-    data = r.json().get('data')
-    children = data.get('children')
-
-    for child in children:
-        title = child.get('data').get('title').lower()
-        for word in counts.keys():
-            counts[word] += title.count(word)
-
-    after = data.get('after')
-
-    if after:
-        count_words(subreddit, word_list, after, counts)
-    else:
-        sorted_word_count = sorted(
-            counts.items(), key=lambda x: (-x[1], x[0].lower()))
-        for item in sorted_word_count:
-            if item[1] != 0:
-                print('{}: {}'.format(item[0], item[1]))
+    sorted_word_count = sorted(
+        word_count.items(), key=lambda x: x[1], reverse=True)
+    for item in sorted_word_count:
+        if item[1] != 0:
+            print('{}: {}'.format(item[0], item[1]))
 
 
-# if __name__ == '__main__':
-#     if len(sys.argv) < 3:
-#         print("Usage: python script.py subreddit word1,word2,word3")
-#         sys.exit(1)
-
-#     subreddit = sys.argv[1]
-#     word_list = sys.argv[2].split(",")
-#     count_words(subreddit, word_list)
+if __name__ == '__main__':
+    count_words(sys.argv[1], sys.argv[2:])
