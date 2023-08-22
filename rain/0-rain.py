1e8f807bf5151c07a3107a7f589b7acf5d8d25f1
Peque <ezequielcabrera2601@gmail.com>
2023-08-22 19:53:07 -0300

    Fixed error

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -9,7 +9,7 @@ def rain(walls):
     calculates how many square units of water will be retained after it rains.
     """
     water = 0
-    for i in range(len(walls) - 1):
+    for i in range(len(walls)):
         for j in range(i + 1, len(walls)):
             if walls[j] > walls[i]:
                 for k in range(i + 1, j):
@@ -19,7 +19,7 @@ def rain(walls):
                         walls[k] += walls[i] - walls[k]
 
     walls.reverse()
-    for i in range(len(walls) - 1):
+    for i in range(len(walls)):
         for j in range(i + 1, len(walls)):
             if walls[j] > walls[i]:
                 for k in range(i + 1, j):
